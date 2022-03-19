6f3f1934bf37f693394d23e5df6d19e02089b4a7
Lomakin Michael <lopler19@mail.ru>
2022-03-19 19:38:12 +0300

    code: добавлен вывод минимума

--- project/project.cpp
+++ project/project.cpp
@@ -8,10 +8,15 @@ int main()
 	cout << "Enter A and B: ";
 	int a, b;
 	cin >> a >> b;
+	int min = a;
+	if (b <= a) {
+		min = b;
+	}
 	cout << "A + B = " << a + b << '\n'
 		<< "A - B = " << a - b << '\n'
 		<< "A * B = " << a * b << '\n'
-		<< "A / B = " << a / b << '\n';
+		<< "A / B = " << a / b << '\n'
+		<< "min = " << min << '\n';
 }
 
 
